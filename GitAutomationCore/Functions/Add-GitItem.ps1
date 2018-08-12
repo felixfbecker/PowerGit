a92bbac55e67d11347887c9ba94e53e53d288c9f
Felix Becker <felix.b@outlook.com>
2018-08-12 19:42:48 +0200

    Fix Add-GitItem for Windows

--- GitAutomationCore/Functions/Add-GitItem.ps1
+++ GitAutomationCore/Functions/Add-GitItem.ps1
@@ -98,7 +98,7 @@ function Add-GitItem {
 
             $pathItem = (Resolve-RealPath $pathItem)
 
-            $strComparison = if ($IsWindows) { [stringcomparison]::InvariantCultureIgnoreCase } else { [stringcomparison]::InvariantCulture }
+            $strComparison = if ($PSVersionTable.PSVersion.Major -lt 6 -or $IsWindows) { [stringcomparison]::InvariantCultureIgnoreCase } else { [stringcomparison]::InvariantCulture }
             if (-not $pathItem.StartsWith($repo.Info.WorkingDirectory, $strComparison)) {
                 Write-Error -Message ('Item ''{0}'' can''t be added because it is not in the repository ''{1}''.' -f $pathItem, $repo.Info.WorkingDirectory)
                 continue
