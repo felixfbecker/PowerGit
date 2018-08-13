fef8934012c8dfa760c63b08ddca073bab3c7e1a
Felix Becker <felix.b@outlook.com>
2018-08-13 17:42:19 +0200

    Add Configuration parameter to build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,11 @@
+param(
+    [ValidateSet('Debug', 'Release')]
+    $Configuration = 'Debug'
+)
+
 Push-Location ./Source/PowerGit
 try {
-    dotnet publish -o "$PSScriptRoot/PowerGit/Assemblies"
+    dotnet publish -o "$PSScriptRoot/PowerGit/Assemblies" -c $Configuration
     if ($LASTEXITCODE -ne 0) {
         throw 'Build failed'
     }
