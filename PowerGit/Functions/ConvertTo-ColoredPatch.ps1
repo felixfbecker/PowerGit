926f10479aa4b4efd6e7e20c5dc102503d21ea84
Felix Becker <felix.b@outlook.com>
2018-09-09 17:51:19 +0200

    refactor: use `e instead of $([char]0x001b)

--- PowerGit/Functions/ConvertTo-ColoredPatch.ps1
+++ PowerGit/Functions/ConvertTo-ColoredPatch.ps1
@@ -6,13 +6,13 @@ function ConvertTo-ColoredPatch {
     )
     ($Patch -split "`n" | ForEach-Object {
             if ($_.StartsWith('-')) {
-                "$([char]0x001b)[31m$_$([char]0x001b)[0m"
+                "`e[31m$_`e[0m"
             } elseif ($_.StartsWith('+')) {
-                "$([char]0x001b)[32m$_$([char]0x001b)[0m"
+                "`e[32m$_`e[0m"
             } elseif ($_.StartsWith('@@')) {
-                $_ -replace '@@(.+)@@', "$([char]0x001b)[36m@@`$1@@$([char]0x001b)[0m"
+                $_ -replace '@@(.+)@@', "`e[36m@@`$1@@`e[0m"
             } elseif ($_.StartsWith('diff ') -or $_.StartsWith('index ') -or $_.StartsWith('--- ') -or $_.StartsWith('+++ ')) {
-                "$([char]0x001b)[1m$_$([char]0x001b)[0m"
+                "`e[1m$_`e[0m"
             } else {
                 $_
             }
