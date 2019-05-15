7b089d6727335566acea8a7bc9e853a2578b80d6
Felix Becker <felix.b@outlook.com>
2019-05-14 23:18:32 -0700

    use global

--- Tests/PowerGitTest/PowerGitTest.psm1
+++ Tests/PowerGitTest/PowerGitTest.psm1
@@ -11,7 +11,7 @@
 # limitations under the License.
 
 Import-Module -Force "$PSScriptRoot/../../PowerGit/Functions/Resolve-RealPath.ps1"
-$testDir = New-Item -ItemType Directory -Path (Join-Path ([IO.Path]::GetTempPath()) ('PowerGitTest-' + [Guid]::NewGuid())) | Resolve-RealPath | Get-Item
+$global:testDir = New-Item -ItemType Directory -Path (Join-Path ([IO.Path]::GetTempPath()) ('PowerGitTest-' + [Guid]::NewGuid())) | Resolve-RealPath | Get-Item
 
 function Add-GitTestFile {
     [CmdletBinding()]
@@ -55,12 +55,11 @@ function New-GitTestRepo {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
     param()
 
-    $testDir = (Resolve-TestDrivePath)
-    $repoRoot = Join-Path -Path $testDir -ChildPath ('PowerGit.{0}' -f ([IO.Path]::GetRandomFileName()))
+    $repoRoot = Join-Path -Path $global:testDir -ChildPath ('PowerGit.{0}' -f ([IO.Path]::GetRandomFileName()))
     New-GitRepository -Path $repoRoot | Format-List | Out-String | Write-Debug
     return $repoRoot
 }
 
 function Resolve-TestDrivePath {
-    $testDir
+    $global:testDir
 }
