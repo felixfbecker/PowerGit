8e61cdd302f7521c2fd719deaf95bbee91fa4679
Felix Becker <felix.b@outlook.com>
2020-06-19 17:26:15 +0200

    feat: support piping in PSGitHub repositories

--- PowerGit/Functions/Add-GitRemote.ps1
+++ PowerGit/Functions/Add-GitRemote.ps1
@@ -1,21 +1,33 @@
 function Add-GitRemote {
+    <#
+    .INPUTS
+    PSGitHub.Repository. You can pipe the output of Get-GitHubRepository, New-GitHubRepository or Start-GitHubFork.
+    #>
     [CmdletBinding()]
     [OutputType([LibGit2Sharp.Remote])]
     param (
         [string] $RepoRoot,
 
         # Name of the new remote
-        [Parameter(Mandatory, Position = 0, ValueFromPipelineByPropertyName)]
+        [Parameter(Mandatory, Position = 0, ParameterSetName = 'Parameters')]
         [ValidateNotNullOrEmpty()]
         [string] $Name,
 
         # URL for the new remote
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory, Position = 1, ParameterSetName = 'Parameters')]
         [ValidateNotNullOrEmpty()]
-        [string] $Url
+        [string] $Url,
+
+        [Parameter(Mandatory, ValueFromPipeline, DontShow, ParameterSetName = 'Input')]
+        [psobject] $Input
     )
 
     process {
+        # PSGitHub support
+        if ($PSCmdlet.ParameterSetName -eq 'Input') {
+            $Name = $Input.Owner
+            $Url = $Input.CloneUrl
+        }
         $repo = Find-GitRepository -Verify
         if (-not $repo) {
             return
